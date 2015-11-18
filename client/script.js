28138333fb5ef8165060b45b67329cc8b424a216
pbhandar2 <bhandaripranav94@gmail.com>
2015-11-18 12:36:21 -0500

    add change color

--- client/script.js
+++ client/script.js
@@ -1,13 +1,16 @@
 
 Meteor.startup(function () {
 	$('.submit-btn').click(function() {
-				event.preventDefault();
-		var exclamation = $('[id=exclamation]').val();
-		var adverb = $('[id=adverb]').val();
-		var noun = $('[id=noun]').val();
-		var verb = $('[id=verb]').val();
-		var madLib = "\'"+exclamation+"\'"+" he said,as he " +adverb+" jumped onto his "+noun+ " and "+verb+" into the sunset.";
+		event.preventDefault();
+		let exclamation = $('[id=exclamation]').val();
+		let adverb = $('[id=adverb]').val();
+		let noun = $('[id=noun]').val();
+		let verb = $('[id=verb]').val();
+		let madLib = "\'"+exclamation+"\'"+" he said,as he " +adverb+" jumped onto his "+noun+ " and "+verb+" into the sunset.";
 
+		$('.text').hide();
+		$('#mad-lib-console').text(madLib).addClass('color');
+		$('.text').fadeIn(1000);
 		console.log(madLib);
 	});
 });
