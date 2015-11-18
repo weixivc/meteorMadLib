8eb73b42123f625c78a697bf1874fcef1d401c1d
pbhandar2 <bhandaripranav94@gmail.com>
2015-11-18 12:16:49 -0500

    add madlib

--- client/script.js
+++ client/script.js
@@ -1,8 +1,14 @@
 
 Meteor.startup(function () {
 	$('.submit-btn').click(function() {
-		event.preventDefault();
-		console.log("Hello World");
+				event.preventDefault();
+		var exclamation = $('[id=exclamation]').val();
+		var adverb = $('[id=adverb]').val();
+		var noun = $('[id=noun]').val();
+		var verb = $('[id=verb]').val();
+		var madLib = "\'"+exclamation+"\'"+" he said,as he " +adverb+" jumped onto his "+noun+ " and "+verb+" into the sunset.";
+
+		console.log(madLib);
 	});
 });
 
